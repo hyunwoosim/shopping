05bcf05b4808c8a7a0f2dd2701cb9caf966a7956
hyunwoo <kshsim98@naver.com>
2024-11-18 23:45:19 +0900

    복습

---
+++ src/main/java/com/hw/shopping/BasicController.java
@@ -0,0 +1,34 @@
+package com.hw.shopping;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    String hello() {
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String about() {
+        return "소개페이지입니다.";
+    }
+
+    @GetMapping("/mypage")
+    @ResponseBody
+    String myPage() {
+        return "마이페이지입니다.";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String date() {
+        return ZonedDateTime.now().toString();
+    }
+}
